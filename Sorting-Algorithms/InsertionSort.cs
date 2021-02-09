ee1e8996f7f95fe6471cc031761d45932ae1c854
Fabian <fabian.tilmans@gmx.de>
2021-02-09 15:13:35 +0100

    Added InsertionSort, Added SelectionSort

---
+++ Sorting-Algorithms/InsertionSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorting_Algorithms
+{
+    public static class InsertionSort
+    {
+        public static int[] Sort(int[] arr)
+        {
+            return Insertionsort(arr);
+        }
+
+        private static int[] Insertionsort(int[] arr)
+        {
+            for (var i = 1; i < arr.Length; ++i)
+            {
+                var key = arr[i];
+                var j = i - 1;
+
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    --j;
+                }
+
+                arr[j + 1] = key;
+            }
+
+            return arr;
+        }
+    }
+}
---
+++ Sorting-Algorithms/SelectionSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorting_Algorithms
+{
+    public static class SelectionSort
+    {
+        public static int[] Sort(int[] arr)
+        {
+            return Selectionsort(arr);
+        }
+
+        private static int[] Selectionsort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                var minIndex = i;
+                for (var j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                var temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+                
+            }
+
+            return arr;
+        }
+    }
+}
